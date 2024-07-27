4923b9c0cfa6bf3ef7fc79a6e6af85ada254a4e8
AbhiUd <abhishekudaiyar464@gmail.com>
2024-07-27 13:45:19 +0530

    Auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,5 +6,26 @@ const bcrypt = require("bcryptjs")
 const SignUp = async (req,res) => {
     const {fname,lname,email,password,phoneno} = req.body
 
+    if (!fname && !lname && !phoneno && !email && !password) {
+        return res.status(400).json({ message: "All fields must be provided." });
+    }
+    
+    try{
+        const Inviteexist = await prisma.Invite.findUnique({
+            where: {
+                email: email,
+              }
+        })
+        if(!Inviteexist){
+          return res.status(404).json({message: "Invite Invalid"})
+        }
+        
+        hashedPassword = bcrypt.hashSync(password)
+        
+        
 
+    }
+    catch(error){
+        
+    }
 }
