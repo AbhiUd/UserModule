c6e4987a165ddab32cb10428862ffe70d784e9ed
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-28 17:43:33 +0530

    generate token

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken")
+const {secret , expiresIn} = require("../config/jwtConfig")
+
+const generate_token = (user) => {
+    const payload = {
+        id : user.id,
+        fname : user.fname,
+        lname : user.lname,
+        email : user.email,
+        password : user.password,
+        mobile_number : user.mobile_number,
+        role : user.role
+    }
+
+    return jwt.sign(payload , secret , {expiresIn})
+}
+
+module.exports = generate_token
