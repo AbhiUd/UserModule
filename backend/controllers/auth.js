193fa6d5bf7c1e60944cc8001f71cbc61357ecd3
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-29 20:25:13 +0530

    auth

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,6 +2,7 @@ const {PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
+const {generate_otp } = require("./email_otp_verify")
 
 const SignUp = async (req,res) => {
     const {fname,lname,email,password,phoneno} = req.body
@@ -20,25 +21,42 @@ const SignUp = async (req,res) => {
           return res.status(404).json({message: "Invite Invalid"})
         }
         
-        
+        const hashedPassword = bcrypt.hashSync(password)
+
         const user = await prisma.userLogin.create({
             data: [
                 {
                     fname: fname,
                     lname:lname,
-                    password: password,
+                    password: hashedPassword,
                     email: email,
                     mobile_number: phoneno
                 }
             ]
         })
+        
         if(!user){
             return res.status(400).json("User not created")
         }
+
+        if(!generate_otp){
+            await prisma.userLogin.delete({
+                where : {
+                    id : user.id
+                }
+            })
+
+            return res.status(404).json({message : "Otp verification unsuccessful"})
+        }
+
         return res.status(201).json({ message: "User created successfully", user });
 
     }
     catch(error){
         return res.status(500).json({ message: "Internal server error", error });
     }
+}
+
+module.exports = {
+    SignUp
 }
