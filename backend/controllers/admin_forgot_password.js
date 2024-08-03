d299c2047c13fbf3c34b2811f89cce37be07c353
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-03 12:57:35 +0530

    admin forgot password

---
+++ backend/controllers/admin_forgot_password.js
@@ -0,0 +1,34 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcryptjs = require("bcryptjs");
+
+const forgot_password = async (req, res) => {
+    const { new_password, confirm_password } = req.body
+    const { email } = req.params
+
+    try {
+        if (new_password != confirm_password) {
+            return res.status(400).json({ message: "Passwords do not match" })
+        }
+
+        const hash_password = bcryptjs.hashSync(confirm_password);
+
+        const update_password = await prisma.adminLogin.update({
+            where: { email: email },
+            data: { password: hash_password }
+        })
+
+        if (update_password) {
+            return res.status(200).json({ message: "Password updated successfully" })
+        }
+
+        return res.status(500).json({ message: "Failed to update password" })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+module.exports = { 
+    forgot_password 
+}
