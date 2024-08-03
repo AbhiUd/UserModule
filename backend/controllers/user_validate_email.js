0644e24d518450091223aeb84e82ae6cc41ad70d
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-03 12:56:55 +0530

    user validate email

---
+++ backend/controllers/user_validate_email.js
@@ -0,0 +1,25 @@
+const {PrismaClient} = require("@prisma/client")
+const prisma = new PrismaClient()
+
+const validate_email = async(req,res) => {
+    const {email} = req.body
+
+    try {
+        const check_email = await prisma.userLogin.findUnique({
+            where : {
+                email : email
+            }
+        })
+
+        if(!check_email){
+            return res.status(404).json({message : "Email id not found"})
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    validate_email
+}
