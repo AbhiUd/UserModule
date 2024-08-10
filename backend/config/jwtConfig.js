342211bbfdd4f00e3ee0735748a8aa94ab9e2bf5
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-11 00:42:48 +0530

    jwt config

--- backend/config/jwtConfig.js
+++ backend/config/jwtConfig.js
@@ -2,6 +2,5 @@ require("dotenv").config()
 
 module.exports = {
     reset_secret : process.env.RESET_SECRET_KEY,
-    secret : process.env.JWT_SECRET_KEY,
-    expiresIn : '1h'
+    secret : process.env.JWT_SECRET_KEY
 }
