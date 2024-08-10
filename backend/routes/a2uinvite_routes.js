d2f767d801b82db036bf02cb740dba27eaee1048
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-11 00:45:54 +0530

    admin to user invite routing

--- backend/routes/a2uinvite_routes.js
+++ backend/routes/a2uinvite_routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const invite_control = require("../controllers/a2u_invite");
+const {auth_middleware} = require("../middleware/auth")
 const invite_router = express.Router(); 
 
-invite_router.post("/admin_signIn/:token/create_invite", invite_control.create_user_invite);
+invite_router.post("/admin_signIn/:token/create_invite",auth_middleware, invite_control.create_user_invite);
 
 module.exports = invite_router
