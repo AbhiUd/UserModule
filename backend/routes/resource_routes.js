427b5d3ebaa72c82ee1b9f2403643f76da920197
AbhiUd <abhishekudaiyar464@gmail.com>
2024-08-15 12:48:02 +0530

    Create resource routing for User and Admin

--- backend/routes/resource_routes.js
+++ backend/routes/resource_routes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const resource_controller = require("../controllers/resources")
 const {auth_middleware} = require("../middleware/auth")
+const resource_map = require("../middleware/map_res2ug")
 const resource_router = express.Router()
 
 
-resource_router.post("/admin_signIn/:token/create_resource",auth_middleware,resource_controller.create_resource)
-resource_router.post("/sa_signin/:token/organizations/organizationId/create_resource",auth_middleware,resource_controller.create_resource)
+resource_router.post("/admin_signIn/:token/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
+resource_router.post("/sa_signin/:token/organizations/organizationId/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
 
 
 module.exports = resource_router
