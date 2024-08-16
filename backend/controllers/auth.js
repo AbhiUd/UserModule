84e6346a720ec5c923ef5528513b9f2528334fa0
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-16 21:29:29 +0530

    user login controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -21,7 +21,7 @@ const transporter = nodemailer.createTransport({
 const SignUp = async (req, res) => {
   const { fname, lname, email, password, mobile_number } = req.body;
 
-  if (!fname && !lname && !mobile_number && !email && !password) {
+  if (!fname || !lname || !mobile_number || !email || !password) {
     return res.status(400).json({ message: "All fields must be provided." });
   }
 
@@ -31,6 +31,7 @@ const SignUp = async (req, res) => {
         email: email,
       },
     });
+
     if (!Inviteexist) {
       return res.status(404).json({ message: "Invite Invalid" });
     }
@@ -38,59 +39,57 @@ const SignUp = async (req, res) => {
     const hashedPassword = bcrypt.hashSync(password);
 
     const user = await prisma.userLogin.create({
-      data: 
-        {
-          fname: fname,
-          lname: lname,
-          password: hashedPassword,
-          email: email,
-          mobile_number: mobile_number,
-          organizationId: Inviteexist.organizationId,
-          roleId: 3
-        },
+      data: {
+        fname: fname,
+        lname: lname,
+        password: hashedPassword,
+        email: email,
+        mobile_number: mobile_number,
+        organizationId: Inviteexist.organizationId,
+        roleId: 3,  
+        // usergroupid: 2
+      },
     });
 
     if (!user) {
       return res.status(400).json("User not created");
     }
 
-    const otp = otp_generator.generate(6 , {upperCaseAlphabets : false , lowerCaseAlphabets : false , specialChars : false})
+    const otp = otp_generator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
 
-    const expiresAt = new Date(Date.now()+1*60*1000)
+    const expiresAt = new Date(Date.now() + 1 * 60 * 1000);
 
     const save_otp = await prisma.otp_schema.create({
-        data : {
-            email,
-            otp,
-            expiresAt
-        }
-    })
-    
+      data: {
+        email,
+        otp,
+        expiresAt,
+      },
+    });
+
     const mailOptions = {
-        from: process.env.SEND_EMAILID,
-        to: email,
-        subject: "Verification Code",
-        html: `<h1>This is the verification code</h1><p>Verification code is : ${otp}`
+      from: process.env.SEND_EMAILID,
+      to: email,
+      subject: "Verification Code",
+      html: `<h1>This is the verification code</h1><p>Verification code is : ${otp}</p>`,
     };
 
     transporter.sendMail(mailOptions, function (err, info) {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ message: "Failed to send email" });
-        } else {
-            console.log(`Email Sent`);
-            return res.status(200).json({ message: "Invite sent successfully" });
-        }
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Failed to send email" });
+      } else {
+        console.log(`Email Sent`);
+        return res.status(200).json({ message: "Invite sent successfully" });
+      }
     });
 
-  
     return res.status(201).json({ message: "User created successfully", user });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({ message: "Internal server error", error });
   }
 };
-
 
 
 const SignIn = async (req, res) => {
