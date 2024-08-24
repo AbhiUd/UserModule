01b08326a6f05f3382dd7822c049bd6271d2a39e
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-24 19:13:22 +0530

    resources routing

--- backend/routes/resource_routes.js
+++ backend/routes/resource_routes.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const resource_controller = require("../controllers/resources")
 const {auth_middleware} = require("../middleware/auth")
-const resource_map = require("../middleware/map_res2ug")
-const resource_router = express.Router()
+const {resource_map} = require("../middleware/map_res2ug")
+const a_resource_router = express.Router()
+const sa_resource_router = express.Router()
+
+a_resource_router.post("/admin_signIn/:token/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
+sa_resource_router.post("/sa_signin/:token/organizations/organizationId/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
 
 
-resource_router.post("/admin_signIn/:token/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
-resource_router.post("/sa_signin/:token/organizations/organizationId/create_resource",auth_middleware,resource_controller.create_resource , resource_map)
-
-
-module.exports = resource_router
+module.exports = {
+    a_resource_router,
+    sa_resource_router
+}
