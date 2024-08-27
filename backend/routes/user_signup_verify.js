ea8ed68fcc8f179afd6358b59fe2d954788a903e
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-27 17:49:50 +0530

    user signup verify routing

---
+++ backend/routes/user_signup_verify.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const user_verify_control = require("../controllers/email_otp_verify")
+
+const user_verify_router = express.Router()
+
+user_verify_router.get("/user_signUp/:email/user_verify",user_verify_control.verify_otp)
+
+module.exports = user_verify_router
