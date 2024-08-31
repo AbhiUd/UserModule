a9719e10b9a62c3a6205da36af925b2939129308
sachdevabhavya <sach.bhavya@gmail.com>
2024-08-31 21:05:40 +0530

    update resource routing

--- backend/routes/update_resource_routing.js
+++ backend/routes/update_resource_routing.js
@@ -2,7 +2,12 @@ const express = require("express");
 const update_control = require("../controllers/update_resource");
 const {auth_middleware} = require("../middleware/auth")
 const update_router = express.Router(); 
+const a_update_router = express.Router()
 
-update_router.put("/user_signUp/:userId/:ug_id/getAllResources/:resource_id/update_resource",auth_middleware , update_control.updateResource)
+update_router.put("/user_signIn/:token/getAllResources/:resourceId/update_resource",auth_middleware , update_control.updateResource)
+a_update_router.put("/admin_signIn/:token/getAllResources/:resourceId/update_resource",auth_middleware , update_control.a_update_resource)
 
-module.exports = update_router
+module.exports = {
+    update_router,
+    a_update_router
+}
